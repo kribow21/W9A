43241a9acb6936ca49228a93b676a686677ad7c3
kribow21 <kristininnotech@gmail.com>
2021-07-09 17:54:33 -0600

    created two arrays and made a loop to run through them

--- js/app.js
+++ js/app.js
@@ -1,22 +1,37 @@
-var userAge = 18;
-var is_subscribed = false;
+//Setting arrays up and variables for loop;
+var userAge = [30,16,18,21,14];
+var is_subscribed = [false,false, true, true, false];
+var counter = 0;
+var length = userAge.length;
+
+//establishing while loop;
+
+while (counter < length){
+
+    if (userAge[counter] < 18 && is_subscribed[counter] == true){
+        console.log("The user is younger than 18 and is subscribed");
+    }
+    else if (userAge [counter]< 18 && (is_subscribed [counter] == false)){
+        console.log("The user is younger than 18 and is not subscribed");
+    }
+    else if (userAge [counter] >= 18 && is_subscribed [counter] == true){
+        console.log("The user is 18 or older and is subscribed")
+    }
+    else if (userAge [counter] >= 18 && (is_subscribed [counter] == false)){
+        console.log("The user is 18 or older and is not subscribed");
+    }
+    else{
+        console.log("Thanks for visiting my website");
+    }
+    counter = counter + 1;
+}
 
 
-if (userAge < 18 && is_subscribed == true){
-    console.log("The user is younger than 18 and is subscribed");
-}
-else if (userAge < 18 && (is_subscribed == false)){
-    console.log("The user is younger than 18 and is not subscribed");
-}
-else if (userAge >= 18 && is_subscribed == true){
-    console.log("The user is 18 or older and is subscribed")
-}
-else if (userAge >= 18 && (is_subscribed == false)){
-    console.log("The user is 18 or older and is not subscribed");
-}
-else{
-    console.log("Thanks for visiting my website");
-}
+
+//variables from first assignment A;
+// var userAge = 18;
+// var is_subscribed = false;
+
 
 // My extra practice attempt at nesting
 // if(userAge < 18){
